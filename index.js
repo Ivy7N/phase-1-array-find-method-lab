0b87230fbf0c791cca4f41e3e8c097d99246b90a
Ivy7N <ivy.ngatia@student.moringaschool.com>
2023-09-28 14:40:57 +0300

    complete array find

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function superbowlWin(records) {
+    const winningRecord = records.find(record => record.result === "W");
+    return winningRecord ? winningRecord.year : undefined;
+  }
+  const superBowlRecords = [
+    { year: 2015, result: "W" },
+    { year: 2014, result: "N/A" },
+    { year: 2013, result: "L" },
+  ];
+  
+  const winningYear = superbowlWin(superBowlRecords);
+  console.log(winningYear); // Output: 2013
+  
+  
+  
+
